cede090147ab0a83bf337528bc73f03cc5951370
Tj <88150681+alditj@users.noreply.github.com>
2025-09-13 23:02:59 +0700

    Update cancel auto shutdown.bat

--- cancel auto shutdown.bat
+++ cancel auto shutdown.bat
@@ -1,17 +1,2 @@
-@echo off
-REM Tempat Sampah - Program Penonaktif
-
-REM Tampilkan dialog konfirmasi
-echo Apakah Anda yakin ingin menonaktifkan program auto-restart? (y/n)
-set /p pilihan=Masukkan pilihan Anda: 
-
-REM Cek input pengguna
-if /i "%pilihan%"=="y" (
-    REM Buat file disable_restart.txt sebagai tanda nonaktif
-    echo Program dinonaktifkan pada %date% %time% > C:\Setup\disable_restart.txt
-    echo Program auto-restart berhasil dinonaktifkan.
-    pause
-) else (
-    echo Program auto-restart tetap aktif.
-    pause
-)
+@echo cencel
+shutdown -a
