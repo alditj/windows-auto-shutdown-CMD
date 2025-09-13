227b5e5c4eeb40b81a9201c9d8e22d0076bc35eb
Tj <88150681+alditj@users.noreply.github.com>
2025-09-13 23:02:35 +0700

    Update click to shutdown.bat

--- click to shutdown.bat
+++ click to shutdown.bat
@@ -1,15 +1,4 @@
 @echo off
-REM Restart Windows - Program Utama
+set waktu=6600
 
-REM Cek apakah program telah dinonaktifkan
-if exist C:\Setup\disable_restart.txt (
-    echo Program dinonaktifkan. Tidak ada tindakan yang dilakukan.
-    exit
-)
-
-REM Tunggu 4 menit (240 detik)
-timeout /t 240 /nobreak
-
-REM Restart Windows
-echo Melakukan restart...
-shutdown /r /t 0
+shutdown /s /t %waktu%
